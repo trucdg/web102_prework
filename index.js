77fef04217d8339c8f2f1fde9f81ca924ce6dda2
Truc Duong <trucdg@bu.edu>
2023-01-18 19:52:45 -0500

    add more description of the games using template literals ans ternary operator

--- index.js
+++ index.js
@@ -150,12 +150,18 @@ allBtn.addEventListener('click', showAllGames);
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+const unfundedNum = GAMES_JSON.filter((game) => game.pledged < game.goal).length;
 
 // create a string that explains the number of unfunded games using the ternary operator
 
+const displayStr =
+    ` A total of $${totalRaised.toLocaleString("en-US")} has been raised for ${GAMES_JSON.length} games.Currently, ${unfundedNum} ${(unfundedNum > 1) ? "games remain" : "game remains"} unfunded.We need your help to fund these amazing games!  
+`
 
 // create a new DOM element containing the template string and append it to the description container
+const newParagraph = document.createElement("p");
+newParagraph.innerText = displayStr;
+descriptionContainer.append(newParagraph);
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
